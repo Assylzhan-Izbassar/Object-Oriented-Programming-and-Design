c4ea5137581b4a69567c929d3e5b6a963df348d7
Assylzhan Izbassar <macbook@Assylzhans-MacBook-Pro.local>
2021-11-20 17:11:16 +0600

    extra_upd

---
+++ extra_lab/src/extra_lab/Account.java
@@ -0,0 +1,13 @@
+package extra_lab;
+
+public class Account {
+
+	private String accountNumber;
+	private String status;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ extra_lab/src/extra_lab/Client.java
@@ -0,0 +1,5 @@
+package extra_lab;
+
+public class Client {
+	
+}
